3cd3f2ef9b4daffd5fef1b8d9fa3a077fe87a365
GianniHonda <gianni.stephanie91@gmail.com>
2026-04-03 18:49:39 +0900

    How to mapping JPA entityes

---
+++ src/main/java/io/github/giannihonda/libraryapi/model/Author.java
@@ -0,0 +1,29 @@
+package io.github.giannihonda.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "author", schema = "public")
+@Getter
+@Setter
+public class Author {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", length = 100, nullable = false)
+    private String name;
+
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate dateBirthDate;
+
+    @Column(name = "nationality", length = 50, nullable = false)
+    private String nationality;
+}
