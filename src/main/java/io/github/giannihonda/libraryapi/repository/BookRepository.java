41488ad424ead90ee58fb08e51c6677fc3cbe42e
GianniHonda <gianni.stephanie91@gmail.com>
2026-04-20 18:24:01 +0900

    Variations of declarative search methods

--- src/main/java/io/github/giannihonda/libraryapi/repository/BookRepository.java
+++ src/main/java/io/github/giannihonda/libraryapi/repository/BookRepository.java
@@ -5,6 +5,7 @@ import io.github.giannihonda.libraryapi.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -24,5 +25,8 @@ public interface BookRepository extends JpaRepository<Book, UUID> {
     List<Book> findByTitleAndPrice(String title, BigDecimal price);
 
     // select * from book where title = ? or isbn = ?
-    List<Book> findByTitleOrIsbn(String title, String isbn);
+    List<Book> findByTitleOrIsbnOrderByTitle(String title, String isbn);
+
+    // select * from book where publication_date between ? and ?
+    List<Book> findByPublicationDate(LocalDate start, LocalDate end);
 }
