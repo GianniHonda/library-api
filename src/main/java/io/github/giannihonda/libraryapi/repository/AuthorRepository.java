2180d152a4f1bdb4dde94413c5228f1e17273cfb
GianniHonda <gianni.stephanie91@gmail.com>
2026-08-04 19:37:43 +0900

    Search authors with filter

--- src/main/java/io/github/giannihonda/libraryapi/repository/AuthorRepository.java
+++ src/main/java/io/github/giannihonda/libraryapi/repository/AuthorRepository.java
@@ -3,7 +3,13 @@ package io.github.giannihonda.libraryapi.repository;
 import io.github.giannihonda.libraryapi.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AuthorRepository extends JpaRepository<Author, UUID> {
+
+    List<Author> findByName(String name);
+    List<Author> findByNationality(String nationality);
+    List<Author> findByNameAndNationality(String name, String nationality);
+
 }
--- src/main/java/io/github/giannihonda/libraryapi/service/AuthorService.java
+++ src/main/java/io/github/giannihonda/libraryapi/service/AuthorService.java
@@ -4,6 +4,7 @@ import io.github.giannihonda.libraryapi.model.Author;
 import io.github.giannihonda.libraryapi.repository.AuthorRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -28,5 +29,21 @@ public class AuthorService {
 
     public void delete(Author author){
         repository.delete(author);
+    }
+
+    public List<Author> search(String name, String nationality){
+        if (name != null && nationality != null){
+            return repository.findByNameAndNationality(name, nationality);
+        }
+
+        if (name != null){
+            return repository.findByName(name);
+        }
+
+        if (nationality != null){
+            return repository.findByNationality(nationality);
+        }
+
+        return repository.findAll();
     }
 }
